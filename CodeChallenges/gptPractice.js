93e631d27daeb4d2dc2868854a2a5eed7e63c736
Akash1321 <Sarkarakash992@gmail.com>
2023-02-28 16:27:28 +0530

    feedback updated

--- CodeChallenges/gptPractice.js
+++ CodeChallenges/gptPractice.js
@@ -1,7 +1,7 @@
 // Challenge 1
 // Write a function that takes a string as input and returns the string reversed. And your function should work for strings of any length and should not use the built-in reverse() function.
 
-function reverseString(str) {
+function reverseString1(str) {
     let reversed = "";
     for(let i = str.length - 1; i >= 0; i--){
         reversed +=  str[i];
@@ -10,6 +10,16 @@ function reverseString(str) {
     return reversed
 }
 
+//feedback: One thing to note is that your implementation uses string concatenation (+=) inside the loop, which can be slow for larger strings since it creates a new string with each iteration. A more optimized approach would be to use an array to store the characters and then join them into a string at the end. Here's an example implementation using this approach:
 
-console.log(reverseString("akash"));
+function reverseString2(str) {
+    let reversed = [];
+    for(let i = str.length - 1; i >= 0; i--){
+        reversed.push(str[i]);
+    }
+
+    return reversed.join("")
+}
+
+console.log(reverseString2("akash"));
   
